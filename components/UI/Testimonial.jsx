914a620d9a8551224379d58b97398045d446d786
Raj Rai <rrai1590@gmail.com>
2023-02-20 02:35:10 +0530

    config update

--- components/UI/Testimonial.jsx
+++ components/UI/Testimonial.jsx
@@ -29,7 +29,7 @@ const Testimonial = () => {
 
           <Col lg="6" md="6">
             <SectionSubtitle subtitle="Testimonials" />
-            <h4 className="mt-4 mb-5">What my colleagues say about me</h4>
+            <h4 className="mt-4 mb-5">What my team say about me</h4>
 
             <Slider {...settings}>
               <div className={`${classes.testimonial__item}`}>
